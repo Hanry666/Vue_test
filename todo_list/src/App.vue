91a5f6ce5833b7a5bd3e40a0e9bce010b83b35e5
hanry_pc <1594002939@qq.com>
2022-10-11 16:01:30 +0800

    初始化项目

---
+++ todo_list/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <List></List>
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+
+export default {
+  name: 'App',
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+* {
+  padding:0;
+  margin: 0;
+  box-sizing: border-box;
+}
+li {
+  list-style: none;
+}
+#app {
+  position:relative;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ todo_list/src/components/List.vue
@@ -0,0 +1,64 @@
+<template lang="">
+    <div id="box">
+        <input type="text" placeholder="请输入你的任务名称名称，按回车健确认" @keydown.enter="AddList" v-model="val">
+        <ul>     
+            <li v-for="(p,index) of ShowList" :key="index"><input type="radio" @click="Change" >{{p}} <button @click="">删除</button></li>
+        </ul>
+        <span>已完成</span>
+    </div>
+</template>
+<script>
+export default {
+       name:'List',
+       data(){
+        return {
+            ListArr:[
+               
+            ],
+            val:'',
+            done:0,
+
+        }
+       },
+       methods:{
+         AddList(e){
+            this.ListArr.push(this.val);
+            console.log(this.ListArr);
+            e.target.blur();
+            this.val='';
+         },
+         Change(){
+           this.value=!this.value;
+         }
+       },
+       computed:{
+          ShowList(){
+            console.log(123);
+            return this.ListArr;
+          }
+       }
+}
+</script>
+<style scoped>
+    #box {
+       position: absolute;
+       left: 50%;
+       top: 50%;
+       transform: translate(-50%,-200px);
+    }
+    input {
+        height: 50px;
+        width: 300px;
+    }
+    ul li {
+        font-size: 20px;
+    }
+    ul li input {
+        float: left;
+        height: 20px;
+        width: 20px;    
+    }
+   ul li button {
+    float: right;
+   }
+</style>
